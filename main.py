5b1a08e50dbebac2fc7488a4382410dc9af7c2c2
Popescu Victor <romaniahasmetal@gmail.com>
2026-06-23 08:38:50 +0300

    Filepath object

---
+++ main.py
@@ -0,0 +1,15 @@
+from src import parse_args as pa
+import pandas as pd
+import sys
+import os
+
+
+
+
+while True:
+    user_input = input("> ")
+    if user_input.lower() == "exit":
+        print("Exiting the program.")
+        sys.exit(0)
+    else:
+        pa.main(user_input.split("_"))
---
+++ src/parse_args.py
@@ -0,0 +1,22 @@
+def main(args):
+
+    if len(args) == 0:
+        print("No arguments provided. Please provide a command.")
+        return
+    if args[0] == "file":
+        if args[1] == "select":
+            from src.args_file import Filepath
+            from tkinter import filedialog
+            filepath = filedialog.askopenfilename(title="Select a file", filetypes=[("CSV files", "*.csv")])
+            if filepath:
+                print(f"Selected file: {filepath}")
+                selected_file = Filepath(filepath)
+                return selected_file
+            else:
+                print("No file selected.")
+                return None
+        if args[1] == "path":
+            if Filepath is not None:
+                print(f"Current file path: {Filepath}")
+            else:
+                print("No file path set.")
