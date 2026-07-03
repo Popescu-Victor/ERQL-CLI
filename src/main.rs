2014780f069a485e8d0c953fe28e1c77f0ed285e
Popescu Victor <romaniahasmetal@gmail.com>
2026-07-03 08:37:13 +0300

    Replacing fn with struct/impl for storing filepath in memory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Write};
+use std::path::PathBuf;
+
 
 mod scripts;
 
@@ -13,12 +15,40 @@ fn main() {
         let command = input.trim();
 
         let parts: Vec<&str> = command.split_whitespace().collect();
-        
+
+
         match parts.as_slice() {
             ["file", "select"] => {
                 scripts::select_file();
+            }
+
+            ["exit"] => {
+                println!("Exiting...");
+                break;
             }
             _ => {
                 println!("Unknown command: {}", command);
             }
 }}}
+
+
+struct FilePath {
+    selected_file: Option<PathBuf>,
+}
+
+impl FilePath {
+    fn new() -> Self {
+        FilePath { selected_file: None }
+    }
+
+    fn select_file(&mut self) {
+        self.selected_file = scripts::select_file();
+    }
+
+    fn print_path(&self) {
+        match &self.selected_file {
+            Some(path) => println!("Selected file path: {}", path.display()),
+            None => println!("No file selected."),
+        }
+    }
+}
--- src/scripts.rs
+++ src/scripts.rs
@@ -1,12 +1,24 @@
 use rfd::FileDialog;
 use std::path::PathBuf;
 
-pub fn select_file() -> Option<PathBuf> {
-    let file = FileDialog::new().pick_file();
-    if let Some(path) = &file {
-        println!("Selected file: {}", path.display());
-    } else {
-        println!("No file selected.");
+
+struct FilePath {
+    selected_file: Option<PathBuf>,
+}
+
+impl FilePath {
+    fn new() -> Self {
+        FilePath { selected_file: None }
     }
-    file
+
+    fn select_file(&mut self) {
+        self.selected_file = scripts::select_file();
+    }
+
+    fn print_path(&self) {
+        match &self.selected_file {
+            Some(path) => println!("Selected file path: {}", path.display()),
+            None => println!("No file selected."),
+        }
+    }
 }
