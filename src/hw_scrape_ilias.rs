046bca68c7387e6ad4d881ad8f8b76609b64f503
Popescu-Victor <romaniahasmetal@gmail.com>
2026-07-12 15:45:48 +0300

    Clean up comments in scrape function
    Removed comments for clarity and conciseness.

--- src/hw_scrape_ilias.rs
+++ src/hw_scrape_ilias.rs
@@ -4,13 +4,8 @@ use thirtyfour::prelude::*;
 
 #[tokio::main]
 pub async fn scrape(weblink: &str, localhost: &str) -> WebDriverResult<()> {
-    // Set up Chrome capabilities
     let caps = DesiredCapabilities::edge();
-
-    // Connect to the chromedriver instance running on port 50098
     let driver = WebDriver::new(&format!("http://{}:51228", localhost), caps).await?;
-
-    // Navigate to the provided weblink
     driver.goto(weblink).await?;
 
 
@@ -25,7 +20,6 @@ pub async fn scrape(weblink: &str, localhost: &str) -> WebDriverResult<()> {
         println!("{}", html);
     }
 
-    // Always close the browser session when done
     driver.quit().await?;
 
     Ok(())
